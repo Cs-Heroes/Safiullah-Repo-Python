04aae28e42c42c685baf0287ff0159ebf43efb30
Safiullah Jalalzai <safiullahjalalzai119@gmail.com>
2022-11-06 20:58:55 +0430

    files system is added to project

--- library project/library.py
+++ library project/library.py
@@ -1,139 +1,144 @@
-# class Person:
-#     def __init__(self,name,lastName,phoneNumber,email):
-#         self.name=name
-#         self.lastName=lastName
-#         self.phoneNumber=phoneNumber
-#         self.email=email
+import math
+class Books:
+    def addBook(self):
+        print("---- BOOK REGISTRATION ----")
+        self.name=input("Enter book name: ")
+        self.pages=int(input("Enter book pages: "))
+        self.author=input("Enter author name: ")
+        self.publishYear=input("Enter publish year: ")
+        bookFile=open("books.txt",'a')
+        bookFile.write(self.name+"\n")
+        bookFile.write(str(self.pages)+"\n")
+        bookFile.write(self.author+"\n")
+        bookFile.write(self.publishYear+"\n")
+        bookFile.close()
+
+    def booksDetails(self):
+        bookFile=open("books.txt",'r')
+        allBooks=bookFile.readlines()
+        bookFile.close()
+        d=0
+        number=1
+        print("---- BOOKS DETAILS ----")
+        print("=====================")
+        print("Book Number ("+str(number)+")")
+        print("=====================")
+        for i in range(math.floor(len(allBooks)/4)):
+            print("book name: "+allBooks[d])
+            print("+++++++++++++++++++++\n")
+            print("pages: "+allBooks[d+1])
+            print("+++++++++++++++++++++\n")
+            print("author: "+allBooks[d+2])
+            print("+++++++++++++++++++++\n")
+            print("publish year: "+allBooks[d+3])
+            number+=1
+            print("=======================")
+            print("Book Number ("+str(number)+")")
+            print("=======================\n")
+            d+=4
 
 
-# class Author(Person):
-#     def __init__(self,name,lastName,phoneNumber,email,job):
-#         Person.__init__(self,name,lastName,phoneNumber,email)
-#         self.job=job
+    def booksList(self):
+        bookFile=open("books.txt",'r')
+        allBooks=bookFile.readlines()
+        bookFile.close()
+        d=0
+        print("=======================")
+        print("Books in libarary")
+        print("=======================")
+        for i in range(math.floor(len(allBooks)/4)):
+            print("\nbook name: "+allBooks[d])
+            print("++++++++++++++++++++++")
+            d+=4
+        print("=======================")
+        print("libarary books end")
+        print("=======================")
 
-    
-# class reader(Person):
-#     def __init__(self,name,lastName,phoneNumber):
-#         Person.__init__(self,name,lastName,phoneNumber)
-#         self.barrowedBooks=[]
-    
-#     def getBook(self,book):
-#         self.barrowedBooks.append(book)
-
-
-
-class Book:
-    
-    def __init__(self,name,author,publishYear,pages):
-        self.name=name
-        self.author=author
-        self.publishYear=publishYear
-        self.pages=pages
-
-    
+    def removeBook(self):
+        bookFile=open("books.txt",'r')
+        allBooks=bookFile.readlines()
+        bookFile.close()
+        removeBook=input("Enter name of book to remove: ")
+        removeBook=removeBook+"\n"
+        try:
 
 
-
-
-    
-
-class Library:
-    def __init__(self):
-
-        self.books=[]
-    # def addBook(self):
-    #     bookName=input("Enter Book Name: ")
-    #     authorName=input("Enter author Name: ")
-    #     authorLastName=input("Enter author last name: ")
-    #     authorPhoneNumber=input("Enter author phone number: ")
-    #     authorEmail=input("Enter author email: ")
-    #     authorJob=input("Enter author job: ")
-    #     bookPublishYear=input("Enter book publish Year: ")
-    #     bookPages=int(input("Enter book pages: " ))
-    #     author=Author(authorName,authorLastName,authorPhoneNumber,authorEmail,authorJob)
-    #     book=Book(bookName,author,bookPublishYear,bookPages)
-    #     self.books.append(book)
-
-    def showBooks(self):
-        for book in self.books:
-            print("Book Name: "+book.name)
-            for author in book.author:
-                print("Author name: "+author.name)
-                print("Author last name: "+author.lastName)
-            print("publish year: "+book.publishYear)
-            print("Book pages: "+str(book.pages))
-
-
-
-# library=Library()
-
-
-# print("Enter 1 to add the book")
-# print("Enter 2 to show the book")
-# choice=int(input("Select: "))
-
-# if (choice==1):
-#     library.addBook()
-
-# elif(choice==2):
-#     library.showBooks()
-
-# class Library:
-#     def __init__(self, listOfBooks):
-#         self.books = listOfBooks
-
-#     def displayAvailableBooks(self):
-#         print("Books present in this library are: ")
-#         for book in self.books: 
-#             print(" *" + book)
-    
-#     def borrowBook(self, bookName):
-#         if bookName in self.books:
-#             print(f"You have been issued {bookName}. Please keep it safe and return it within 30 days")
-#             self.books.remove(bookName)
-#             return True
-#         else:
-#             print("Sorry, This book is either not available or has already been issued to someone else. Please wait until the book is available")
-#             return False
-
-#     def returnBook(self, bookName):
-#         self.books.append(bookName)
-#         print("Thanks for returning this book! Hope you enjoyed reading it. Have a great day!")
-
-# class Student: 
-#     def requestBook(self):
-#         self.book = input("Enter the name of the book you want to borrow: ")
-#         return self.book
-
-#     def returnBook(self):
-#         self.book = input("Enter the name of the book you want to return: ")
-#         return self.book
-         
-
-# if __name__ == "__main__":
-#     centraLibrary = Library(["JavaScript", "PHP", "C++", "Python", "Java"])
-#     student = Student()
-    
-#     while(True):
-#         welcomeMsg = '''\n====== Welcome to Simple Library  System======
-#         Please choose an option:
-#         1. List all the books
-#         2. Request a book
-#         3. Return a book
-#         4. Exit the Library
-#         '''
-#         print(welcomeMsg)
-#         a = int(input("Enter a choice: "))
-#         if a == 1:
-#             centraLibrary.displayAvailableBooks()
-#         elif a == 2:
-#             centraLibrary.borrowBook(student.requestBook())
-#         elif a == 3:
-#             centraLibrary.returnBook(student.returnBook())
-#         elif a == 4:
-#             print("Thanks for choosing our Library.")
-#             exit()
-#         else:
-#             print("Invalid Choice!")
+            removeBookIndex=allBooks.index(removeBook)
+       
+            allBooks.remove(removeBook)
+            allBooks.pop(removeBookIndex)
+            allBooks.pop(removeBookIndex)
+            allBooks.pop(removeBookIndex)
+            bookFile=open("books.txt",'w')
+            for i in allBooks:
+                bookFile.write(i)
+        except ValueError:
+            print("we haven't such book\npleace try agian")
 
         
+
+class Student:
+    def addStudent(self):
+        self.id=int(input("Enter student id: "))
+        self.name=input("Enter student name: ")
+        self.lastName=input("Enter student last name: ")
+        self.phone=input("phone number:")
+        students=open("students.txt",'a')
+        students.write(str(self.id)+"\n")
+        students.write(self.name+"\n")
+        students.write(self.lastName+"\n")
+        students.write(self.phone+"\n")
+        students.close()
+
+    def allStudentsDetials(self):
+        studentFile=open("students.txt",'r')
+        allStudents=studentFile.readlines()
+        d=0
+        for i in range(math.floor(len(allStudents)/4)):
+            print("student id: "+allStudents[d])
+            print("student name: "+allStudents[d+1])
+            print("studebt last name "+allStudents[d+2])
+            print("phone number "+allStudents[d+3])
+            
+            print("=======================")
+            d+=4
+
+
+
+    def removestudent(self):
+        students=open("students.txt",'r')
+
+
+
+        
+        
+
+
+
+
+def library():
+    book=Books()
+    
+
+
+    while True:
+        print("\n\n\nWhat do you want:\n1. add book:\n2. Books details:\n3. Books list:\n4. Remove book: \n0. for exit")
+        select=int(input("select: "))
+        print("\n\n")
+
+        if select==1:
+            book.addBook()
+        elif select==2:
+            book.booksDetails()
+        elif select==3:
+            book.booksList()
+        elif select==4:
+            book.removeBook()
+            pass
+        else:
+            exit()
+
+ 
+library()
+
+
